aafb3318198babfdd31a68193ff248b95455f376
douglas <douglasmartinssantos7@gmail.com>
2024-11-04 13:08:53 -0300

    insert dependecie junit 5+

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("io.appium:java-client:8.4.0")
     testImplementation("org.seleniumhq.selenium:selenium-java:4.11.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 tasks.test {
